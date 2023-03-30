f545213eea7ef1843f0dbaabc19862ce680485ea
ilonavarro <ilo.navarro+github@gmail.com>
2023-03-30 17:59:30 +0100

    SSG x SSR

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react'
 import Link from '../src/components/Link'
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   const FAQ_URL_API =
     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
   const dadosFaq = await fetch(FAQ_URL_API)
@@ -18,9 +17,6 @@ export async function getStaticProps(context) {
 }
 
 const faq = ({ dadosFaq }) => {
-  // const [dadosFaq, setDadosFaq] = useState([])
-  // useEffect(() => {
-  // }, [])
   return (
     <div>
       <h1>Alura Cases - Faq</h1>
